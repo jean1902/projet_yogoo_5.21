280803866c49d0f682d2f332034c31be3fa7c3d4
jean1902 <cin71781@gmail.com>
2022-01-21 01:01:30 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let menu_burger=document.querySelector('.btn_menu');
+let link_site =document.querySelector('.linkSite');
+menu_burger.addEventListener('click',()=>{
+    link_site.style="Display:block";
+})
